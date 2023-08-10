f17eb8366de24e276e788ec9ecb2718b19f7ff17
Mahamadou <mmohmo@yahoo.com>
2023-08-10 00:21:32 -0400

    stock list styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { StocksContext } from '../context/stocksContext'
 export const Home = () => {
     const ctx = useContext(StocksContext)
     const { activeStocks } = ctx
-    console.log(activeStocks)
+    
     if(activeStocks === null) {
         return(
             <div> 
@@ -15,34 +15,35 @@ export const Home = () => {
     const companies = activeStocks.map((stock, idx) => <li key={idx}>{stock.name}</li>)
     const changes = activeStocks.map((stock, idx) => <li key={idx}>{stock.change}</li>)
     const price = activeStocks.map((stock, idx) => <li key={idx}> {stock.price}</li>)
+    
     return(
         <div>
             <h2>Home Page</h2>
             <h3>Most Active Stocks</h3>
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid grid-cols-3 gap-2 py-4">
                 <div>
-                    <h2>Company Name</h2>
+                    <h2 className="text-2xl">Company Name</h2>
                 </div>
                 <div>
-                    <h2>Price</h2>
+                    <h2 className="text-2xl">Price</h2>
                 </div>
                 <div>
-                    <h2>Change</h2>
+                    <h2 className="text-2xl">Change</h2>
                 </div>
             </div>
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid grid-cols-3 gap-1">
                 <div>
-                    <u>
+                    <u className="text-sm list-none no-underline divide-y divide-blue-200">
                         {companies}
                     </u>
                 </div>
                 <div>
-                    <ul>
+                    <ul className="text-sm divide-y divide-blue-200">
                         {price}
                     </ul>
                 </div>
                 <div>
-                    <ul>
+                    <ul className="text-sm divide-y divide-blue-200">
                         {changes}
                     </ul>
                 </div>
@@ -51,4 +52,3 @@ export const Home = () => {
         </div>
     )
 }
-
